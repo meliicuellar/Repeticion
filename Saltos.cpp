bd76b6d992787f54e290e9b2af81a9699ca768ac
meliicuellar <melicuellar.22@gmail.com>
2017-05-02 11:46:54 -0300

    Add files via upload

---
+++ Saltos.cpp
@@ -0,0 +1,29 @@
+/*Saltos
+Programa que muestra una frase repetida 200 veces
+Melina Cuellar, Ignacio Muñoz, Ramiro Balsa, Agustín Sebastián, Yamil Lopez
+20170413
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	unsigned i;
+	
+	i = 1;
+	
+	salto: if(i<=200){
+		
+		cout<<"Voy a practicar programacion."<<endl;
+		i++;
+		goto salto;
+		
+	return 0;	
+	}
+	
+	
+	
+	
+}
+ 
