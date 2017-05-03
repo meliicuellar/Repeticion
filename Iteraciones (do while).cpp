881b7637f8f92fb0bd2e30e46d3c710ee174938e
meliicuellar <melicuellar.22@gmail.com>
2017-05-03 10:36:55 -0300

    Add files via upload

---
+++ Iteraciones (do while).cpp
@@ -0,0 +1,22 @@
+/*Iteraciones
+Programa que muestra una frase repetida 200 veces
+Melina Cuellar, Ignacio Muñoz, Ramiro Balsa, Agustín Sebastián, Yamil Lopez
+20170423
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	unsigned i;
+	
+	i = 1;
+	
+	do{
+		cout<<"Voy a practicar programacion."<<endl;
+		i++;	
+	}while(i<=200);
+	
+	return 0;
+}
